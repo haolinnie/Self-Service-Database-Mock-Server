26daaaf5a509d008746cdf57e2aba3f6857a3def
Tiger Nie <nhl0819@hotmail.com>
2019-10-16 13:55:25 -0500

    Removed unused imports

--- db_op.py
+++ db_op.py
@@ -5,3 +5,4 @@ db_path = "data/deid_data.db"
 
 def get_db():
     return sqlite3.connect(db_path)
+
--- server.py
+++ server.py
@@ -1,10 +1,9 @@
 #!flask/bin/python
 import sys
 from flask import Flask, jsonify, Markup
-from flask import make_response, abort
+from flask import make_response
 from flask_restful import Resource, Api, reqparse
 from flask_misaka import markdown
-import json
 
 # Get sample database
 from db_op import get_db
