b027e8cc90327c99f8a1b92314c43dd13aff97c3
Tiger Nie <nhl0819@hotmail.com>
2019-11-21 12:38:55 -0600

    Refactored database wrapper code

--- ssd_api/db/db_api.py
+++ ssd_api/db/db_api.py
@@ -1,4 +1,26 @@
-from .Database import Database as db
+from .Database import Database
 
 
+
+def db_execute(cmd):
+    with Database.get_db().cursor() as cursor:
+        cursor.execute(cmd)
+        res = cursor.fetchall()
+    return res
+
+
+def get_table_names():
+    with Database.get_db().cursor() as cursor:
+        cursor.execute('''SELECT table_name FROM information_schema.tables
+        WHERE table_schema = %s;''', Database.db_name)
+        table_names = cursor.fetchall()
+    return [v[0] for v in table_names]
+
+
+def get_table_columns(table_name):
+    with Database.get_db().cursor() as cursor:
+        cursor.execute('''SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS
+        WHERE table_name = %s;''', table_name)
+        col_names = cursor.fetchall()
+    return [v[0] for v in col_names]
 
--- ssd_api/server.py
+++ ssd_api/server.py
@@ -4,8 +4,9 @@ import sys
 from flask import Flask, jsonify, make_response
 from flask_restful import Resource, Api, reqparse
 
+from . import db as db_utils
+from .db import Database
 from . import config_module
-from .db import Database as db
 from .util import get_documentation
 
 
@@ -18,7 +19,7 @@ parser.add_argument('pt_id', type=int, action='append', help='ERROR: empty pt_id
 
 class TableNames(Resource):
     def get(self):
-        return jsonify({'table_names': db.get_table_names()})
+        return jsonify({'table_names': db_utils.get_table_names()})
 
 
 def check_sql_safe(*argv):
@@ -35,7 +36,7 @@ class GetTable(Resource):
             return jsonify({'ERROR': 'Invalid input'})
 
         try:
-            with db.get_db().cursor() as cursor:
+            with Database.get_db().cursor() as cursor:
                 cursor.execute("SELECT * FROM {}".format(table_name))
                 names = list(map(lambda x: x[0], cursor.description))
                 data = cursor.fetchall()
@@ -59,7 +60,7 @@ class GetTableCols(Resource):
             return jsonify({'ERROR': 'Invalid input'})
 
         try:
-            col_names = db.get_table_columns(table_name)
+            col_names = db_utils.get_table_columns(table_name)
         except Exception as e:
             return {"Exception Type": str(type(e)),
                     "Args": str(e.args),
@@ -85,7 +86,7 @@ class GetDistinctX(Resource):
 
         try:
             cmd = "SELECT DISTINCT {} FROM {}".format(col_name, table_name)
-            data = db.db_execute(cmd)
+            data = db_utils.db_execute(cmd)
             data = [r[0] for r in data]
         except Exception as e:
             return {"Exception Type": str(type(e)),
@@ -120,7 +121,7 @@ class FilterTableWithPTID(Resource):
 
         # Filter table for pt_id
         try:
-            with db.get_db().cursor() as cursor:
+            with Database.get_db().cursor() as cursor:
                 cursor.execute(cmd)
                 col_names = list(map(lambda x: x[0], cursor.description))
                 res = cursor.fetchall()
@@ -154,7 +155,7 @@ class PatientHistory(Resource):
             table_name = "medication_deid"
             cmd = """ SELECT {} FROM {} WHERE pt_id IN({}) ORDER BY order_placed_dt
             """.format(cols, table_name, id)
-            res = db.db_execute(cmd)
+            res = db_utils.db_execute(cmd)
             out_json[str(id)]['medication'] = [dict(zip(med_cols, val)) for val in res]
 
             # Diagnosis
@@ -162,7 +163,7 @@ class PatientHistory(Resource):
             table_name = "diagnosis_deid"
             cmd = """ SELECT {} FROM {} WHERE pt_id IN({}) ORDER BY diagnosis_start_dt 
             """.format(cols, table_name, id)
-            res = db.db_execute(cmd)
+            res = db_utils.db_execute(cmd)
             out_json[str(id)]['eye_diagnosis'] = [dict(zip(diag_cols, val)) for val in res if val[0].find('retina')!=-1]
             out_json[str(id)]['systemic_diagnosis'] = [dict(zip(diag_cols, val)) for val in res if val[0].find('retina')==-1]
 
@@ -171,7 +172,7 @@ class PatientHistory(Resource):
             table_name = "lab_value_deid"
             cmd = """ SELECT {} FROM {} WHERE pt_id IN({}) ORDER BY result_dt
             """.format(cols, table_name, id)
-            res = db.db_execute(cmd)
+            res = db_utils.db_execute(cmd)
             out_json[str(id)]['lab_values'] = [dict(zip(lab_cols, val)) for val in res]
 
             # Vision & Pressure
@@ -179,7 +180,7 @@ class PatientHistory(Resource):
             table_name = "smart_data_deid"
             cmd = """ SELECT {} FROM {} WHERE pt_id IN({}) ORDER BY value_dt
             """.format(cols, table_name, id)
-            res = db.db_execute(cmd)
+            res = db_utils.db_execute(cmd)
             out_json[str(id)]['vision'] = [dict(zip(smart_cols, val)) for val in res if val[0].find('ACUIT') != -1]
             out_json[str(id)]['pressure'] = [dict(zip(smart_cols, val)) for val in res if val[0].find('PRESSURE') != -1]
 
@@ -199,12 +200,12 @@ class PatientImages(Resource):
         
         ## image_procedures cache
         cmd = """SELECT image_procedure_id, image_procedure FROM image_procedure"""
-        res = db.db_execute(cmd)
+        res = db_utils.db_execute(cmd)
         image_procedures = dict(res)
         for id in pt_id:
             cmd = """SELECT exam_id, exam_date FROM exam_deid
             WHERE pt_id IN({}) ORDER BY exam_date """.format(id)
-            res = db.db_execute(cmd)
+            res = db_utils.db_execute(cmd)
             out_cols = ('exam_id', 'exam_date')
             out_json[str(id)] = [dict(zip(out_cols, val)) for val in res]
 
@@ -212,7 +213,7 @@ class PatientImages(Resource):
                 curr_exam['images'] = {}
                 cmd = """SELECT image_id, image_num, image_type, image_laterality, image_procedure_id
                 FROM image_deid WHERE exam_id IN({}) ORDER BY image_num """.format(curr_exam['exam_id'])
-                res = db.db_execute(cmd)
+                res = db_utils.db_execute(cmd)
                 curr_exam['images'] = [dict(zip(image_cols, list(val[:-1]) + [image_procedures[val[-1]]] )) for val in res]
 
         return jsonify(out_json)
@@ -222,11 +223,11 @@ def create_app(config=None):
 
     # Instantiate flask app
     app = Flask(__name__, instance_relative_config=True)
-    db.init_app(app)
+    Database.init_app(app)
 
     if config is not None:
-        db.test = config['test']
-    db.test = False 
+        Database.test = config['test']
+    Database.test = False 
 
     # proxy support for Nginx
     from werkzeug.middleware.proxy_fix import ProxyFix
