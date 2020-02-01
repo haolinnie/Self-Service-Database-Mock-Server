f7b6d2ad256a951a497bbe5d84dcdf812ba0b07e
Tiger Nie <nhl0819@gmail.com>
2020-02-01 10:42:27 -0600

    trav

--- scripts/dev_db/initialise_db.sh
+++ scripts/dev_db/initialise_db.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 sudo docker cp data/sample_db_mssql1.sql mssql1:/tmp
 sudo docker exec -it mssql1 /opt/mssql-tools/bin/sqlcmd \
-   -S 0.0.0.0 -U SA -P "yourStrong_Password" \
+   -S 0.0.0.0 -U "SA" -P "yourStrong_Password" \
    -i '/tmp/sample_db_mssql1.sql'
 
 sudo docker cp data/sample_db_mssql2.sql mssql2:/tmp
 sudo docker exec -it mssql2 /opt/mssql-tools/bin/sqlcmd \
-   -S 0.0.0.0 -U SA -P "yourStrong_Password" \
+   -S 0.0.0.0 -U "SA" -P "yourStrong_Password" \
    -i '/tmp/sample_db_mssql2.sql'
