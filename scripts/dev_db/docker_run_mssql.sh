2f319e7369024eed3b33e9ea7a0b69f42bf00550
Tiger Nie <nhl0819@gmail.com>
2020-02-01 10:33:13 -0600

    trav

--- scripts/dev_db/docker_run_mssql.sh
+++ scripts/dev_db/docker_run_mssql.sh
@@ -7,3 +7,5 @@ sudo docker run --rm -e 'ACCEPT_EULA=Y' -e 'MSSQL_SA_PASSWORD=yourStrong_Passwor
 sudo docker run --rm -e 'ACCEPT_EULA=Y' -e 'MSSQL_SA_PASSWORD=yourStrong_Password' \
    -p 4434:1433 --name mssql2 \
    -d mcr.microsoft.com/mssql/server:2019-GA-ubuntu-16.04
+
+sudo docker ps
